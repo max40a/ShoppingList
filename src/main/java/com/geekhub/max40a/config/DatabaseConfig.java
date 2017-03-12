05fe925d9595b3a12f1bf3b6c134904c69578db6
Retro <max40a@mail.ru>
2017-03-12 19:00:24 +0200

    add .gitignore and gradle.build files.

--- src/main/java/com/geekhub/max40a/config/DatabaseConfig.java
+++ src/main/java/com/geekhub/max40a/config/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package com.geekhub.max40a.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -19,6 +20,7 @@ public class DatabaseConfig {
     @Value("${spring.datasource.password}")
     private String password;
 
+    @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(driver);
