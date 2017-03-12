9ca0dfc39b25c1bc78d094fa6b02e083910b51cd
Retro <max40a@mail.ru>
2017-03-12 17:13:11 +0200

    init commit.

---
+++ src/main/java/com/geekhub/max40a/ShoppingListApplication.java
@@ -0,0 +1,12 @@
+package com.geekhub.max40a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListApplication.class, args);
+	}
+}
