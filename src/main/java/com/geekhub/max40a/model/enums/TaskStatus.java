477c68fc45fd6c3f411cb50dc6defa47aa507472
Retro <max40a@mail.ru>
2017-03-15 18:22:27 +0200

    update TaskStatus enum

--- src/main/java/com/geekhub/max40a/model/enums/TaskStatus.java
+++ src/main/java/com/geekhub/max40a/model/enums/TaskStatus.java
@@ -1,6 +1,16 @@
 package com.geekhub.max40a.model.enums;
 
 public enum TaskStatus {
-    INCOMPLETE,
-    COMPLETE
+    INCOMPLETE("Incomplete"),
+    COMPLETE("Complete");
+
+    private String status;
+
+    TaskStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatusToString() {
+        return status;
+    }
 }
