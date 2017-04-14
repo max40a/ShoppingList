4576eeadde1989041a86afaf9ce17e0702cdfa06
Retro <max40a@mail.ru>
2017-04-14 18:01:20 +0300

    modify TaskResponseDto

--- src/main/java/com/geekhub/max40a/dto/TaskResponseDto.java
+++ src/main/java/com/geekhub/max40a/dto/TaskResponseDto.java
@@ -5,22 +5,19 @@ import com.geekhub.max40a.model.Task;
 import java.util.List;
 
 public class TaskResponseDto {
-    private List<Task> taskList;
-    private Integer countPage;
+    private final List<Task> taskList;
+    private final Integer countPage;
+
+    public TaskResponseDto(List<Task> taskList, Integer countPage) {
+        this.taskList = taskList;
+        this.countPage = countPage;
+    }
 
     public List<Task> getTaskList() {
         return taskList;
     }
 
-    public void setTaskList(List<Task> taskList) {
-        this.taskList = taskList;
-    }
-
     public Integer getCountPage() {
         return countPage;
-    }
-
-    public void setCountPage(Integer countPage) {
-        this.countPage = countPage;
     }
 }
